7ed7d721874acbc4c99af4ceeda02c5e4cd1db6d
OpKSpatY <mcpedrohenriquelc@gmail.com>
2024-07-26 14:03:43 -0300

    feat: config. inicial

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
+import AppRoutes from './routes/AppRoutes';
+
+function App() {
+  return (
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <a href="/about">About</a>
+            </li>
+            <li>
+              <a href="/contact">Contact</a>
+            </li>
+          </ul>
+        </nav>
+        <AppRoutes />
+      </div>
+    </Router>
+  );
+}
+
+export default App;
---
+++ src/pages/Home.js
@@ -0,0 +1,12 @@
+import LoginComponent from "../components/LoginComponent";
+
+function Home() {
+  return (
+    <>
+      <h2>Home Page</h2>
+      <LoginComponent/>
+    </>
+  );
+}
+
+export default Home;
